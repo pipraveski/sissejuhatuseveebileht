29851f8fdbb09e2d060795905cccbf19fd8d1a46
pipraveski <146386198+pipraveski@users.noreply.github.com>
2023-12-04 21:20:55 +0200

    Add files via upload
    agendid

---
+++ uleskerimis-nupp-script.js
@@ -0,0 +1,6 @@
+function scrollToTop() { // Funktsioon lehe algusesse kerimiseks. Koodi kirjutas ChatGPT (https://chat.openai.com/).
+    window.scrollTo({
+      top: 1,
+      behavior: 'smooth'
+    });
+  }
