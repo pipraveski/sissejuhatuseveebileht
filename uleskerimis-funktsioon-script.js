8aea2c7743abc1956481a384f541beaabef1f3fb
pipraveski <146386198+pipraveski@users.noreply.github.com>
2023-12-04 21:22:21 +0200

    agendid

---
+++ uleskerimis-funktsioon-script.js
@@ -0,0 +1,6 @@
+function scrollAlmostTop() { // Funktsioon kirjelduse kõrgusele kerimiseks. Koodi kirjutas ChatGPT (https://chat.openai.com/).
+  window.scrollTo({
+    top: 270,
+    behavior: 'smooth'
+  });
+}
