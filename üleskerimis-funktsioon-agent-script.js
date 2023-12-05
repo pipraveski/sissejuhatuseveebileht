3b5f73150357daca7ba014992553c66ab105e862
Jonathan Astmäe <114855710+Asttar@users.noreply.github.com>
2023-12-05 13:58:38 +0200

    Add files via upload

---
+++ üleskerimis-funktsioon-agent-script.js
@@ -0,0 +1,14 @@
+function scrollAlmostTop() {
+  const targetElement = document.getElementById('agentKirjeldus'); // Replace 'yourTargetElementId' with your actual element ID
+  const offsetPercentage = 10; // Adjust this percentage to scroll higher or lower above the element
+  
+  if (targetElement) {
+    const elementHeight = targetElement.getBoundingClientRect().height;
+    const offset = (elementHeight * offsetPercentage) / 100;
+    const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - offset;
+    window.scrollTo({
+      top: targetPosition,
+      behavior: 'smooth'
+    });
+  }
+}
