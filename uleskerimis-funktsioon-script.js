92125db3e6d10e101ce763c994e6f1e18691b2b5
Jonathan Astmäe <114855710+Asttar@users.noreply.github.com>
2023-12-05 14:01:21 +0200

    Update and rename uleskerimis-funktsioon-script.js to uleskerimis-funktsioon-agent-script.js

--- uleskerimis-funktsioon-script.js
+++
@@ -1,6 +0,0 @@
-function scrollAlmostTop() { // Funktsioon kirjelduse kõrgusele kerimiseks. Koodi kirjutas ChatGPT (https://chat.openai.com/).
-  window.scrollTo({
-    top: 270,
-    behavior: 'smooth'
-  });
-}
